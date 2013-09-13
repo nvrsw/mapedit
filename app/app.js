e267df559d2b661fdb97b829a681a14dada6628c
InCheol Moon <moonpfe@emstone.com>
2013-09-14 03:03:08 +0900

    mapedit: add base menus

--- app/app.js
+++ app/app.js
@@ -1,5 +1,30 @@
-var gui = require("nw.gui");
+var app_gui = require("nw.gui");
+var app_window;
 
 $(function() {
-  gui.Window.get().show();
+  app_window = app_gui.Window.get();
+
+  var menu = new app_gui.Menu({ type: 'menubar' });
+  var submenu;
+
+  submenu = new app_gui.Menu();
+  submenu.append(new app_gui.MenuItem({ label: 'New File' }));
+  submenu.append(new app_gui.MenuItem({ label: 'Open File' }));
+  submenu.append(new app_gui.MenuItem({ label: 'Save' }));
+  submenu.append(new app_gui.MenuItem({ label: 'Save As' }));
+  submenu.append(new app_gui.MenuItem({ label: 'Close File' }));
+  submenu.append(new app_gui.MenuItem({ label: 'Exit',
+                                        click: function() {
+                                          app_window.close(true);
+                                        }
+                                      }));
+
+  menu.append(new app_gui.MenuItem({ label: 'File', submenu: submenu }));
+
+  // attach menu to window
+  app_window.menu = menu;
+  app_window.show();
+
+  // for debugging
+  app_window.showDevTools();
 });
