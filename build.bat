25befd03dc0826dcd7158865aecbfbe377d99782
InCheol Moon <moonpfe@emstone.com>
2014-11-06 17:50:49 +0900

    mapedit: apply node-webkit changes

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ cd app
 move app.zip ..\app.nw
 cd ..
 copy /b node-webkit\win-ia32\nw.exe+app.nw release\app.exe
-copy node-webkit\win-ia32\icudt.dll release\
+copy node-webkit\win-ia32\icudtl.dat release\
 copy node-webkit\win-ia32\nw.pak release\
 mkdir release\samples
 copy samples\* release\samples\
