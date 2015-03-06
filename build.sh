b787e628e32db6138b0a18dfe2c3d2473b9fd2aa
Sunjin Yang <lethean@emstone.com>
2015-03-06 13:55:27 +0900

    mapedit: add release package build script
    아직 버전을 자동으로 인식하지 못하기 때문에 다음처럼
    수동으로 지정해서 실행하면
    
    ```
    $ VERSION="1.2.0" ./build.sh
    ```
    
    윈도우 / 리눅스용 압축파일이 생성됩니다.

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+[ "$VERSION" = "" ] && VERSION='0.9.9'
+[ "$PLATFORMS" = "" ] && PLATFORMS='linux-ia32 linux-x64 win-ia32 win-x64'
+
+rm -rf vms-mapedit*
+
+# Make a package
+rm -f app.nw && cd app && zip -q -r -9 ../app.nw * && cd ..
+
+for platform in $PLATFORMS; do
+  echo -ne "$platform \t==> "
+  nwjs="nwjs/$platform"
+  case $platform in
+    linux-*)
+      suffix=""
+      ;;
+    win-*)
+      suffix=".exe"
+      ;;
+    *)
+      echo "not yet supported platform"
+      exit 1
+      ;;
+  esac
+
+  dir="vms-mapedit-$VERSION-$platform-"$(date +"%Y%m%d")
+  mkdir -p $dir
+
+  cat "nwjs/$platform/nw$suffix" app.nw > "$dir/mapedit$suffix"
+  [ "$suffix" = ".exe" ] || chmod +x "$dir/mapedit$suffix"
+  cp "nwjs/$platform/icudtl.dat" $dir
+  cp "nwjs/$platform/nw.pak" $dir
+
+  cp -fr samples $dir
+  cp -fr ../doc/ChangeLog-mapedit.html $dir/ChangeLog.html
+
+  if [ "$suffix" = ".exe" ]; then
+    echo "$dir".zip
+    zip -q -r -9 "$dir".zip $dir
+  else
+    echo "$dir".tar.xz
+    tar cJf "$dir".tar.xz $dir
+  fi
+
+  rm -rf $dir
+done
